8310f5669da2066c61be686313a0818ddecc5f0f
Giuseppe Moi <giuseppemoi@Giuseppes-MacBook-Air.local>
2021-11-05 16:43:11 +0100

    commit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,19 @@ import React, {useEffect} from 'react'
 import axios from "axios";
 
 const Home = () => {
+    function fetch() {
+        axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?X-CMC_PRO_API_KEY=42501420-56b2-4d9d-862f-d46a4bcf6dac', {
+            headers: { 'X-CMC_PRO_API_KEY': '42501420-56b2-4d9d-862f-d46a4bcf6dac' }
+        })
+            .then(res => {
+                console.log(res)
+            });
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [])
+
     return (
         <div>
             <p>home page</p>
