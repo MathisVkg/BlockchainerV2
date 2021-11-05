2fcc3fce0a3e5ef44347b0d8847547c04a1d0597
MathisVkg <mvk181@gmail.com>
2021-11-05 16:38:01 +0100

    nav fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { IoMdArrowDropright } from 'react-icons/io'
 import { FaAngleLeft } from 'react-icons/fa'
 import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
+    const navBar = useRef(null)
+    const navTest = useRef(null)
     return (
         <div>
-            <span className="openIcon"><GiHamburgerMenu/></span>
-            <nav class="navBar">
-                <span class="backIcon"><FaAngleLeft /></span>
-                <NavLink to='/'>DashBoard</NavLink>
-                <NavLink to='/favorite'>Favorite</NavLink>
-                <NavLink to='/compare'>Compare</NavLink>
-                <div class="menuCrypto">
-                    <div className="menuGroup">
-                        <p class="menuText">Crypto</p>
-                        <span class="menuIcon"><IoMdArrowDropright /></span>
+            <span className="openIcon"><GiHamburgerMenu onClick={ () => { openMenu() } }/></span>
+            <nav className="navBar navOff" ref={ navBar }>
+                <div className="gold">
+                    <div className="black">
+                        <span className="backIcon"><FaAngleLeft onClick={ () => { closeMenu() } }/></span>
+                        <NavLink className="dashBoard" to='/Home' ref={ navBar }>DashBoard</NavLink>
+                        <NavLink to='/favorite'>Favorite</NavLink>
+                        <NavLink to='/compare'>Compare</NavLink>
+                        <NavLink to='/user'>User</NavLink>
+                        <NavLink to='/#'>Cryptocurrency</NavLink>
                     </div>
-                    <NavLink to='/bitcoin'>Bitcoin</NavLink>
-                    <NavLink to='/ethereum'>Ethereum</NavLink>
-                    <NavLink to='/xrp'>XRP</NavLink>
-                    <NavLink to='/cake'>Cake</NavLink>
-                    <NavLink to='/shiba'>Shiba</NavLink>
                 </div>
-                <NavLink to='/user'>User</NavLink>
             </nav>
         </div>
     )
     
+    function openMenu() {
+        navBar.current.classList.remove('navOff')
+        navBar.current.classList.add('navOn')
+    }
+
+    function closeMenu() {
+        navBar.current.classList.remove('navOn')
+        navBar.current.classList.add('navOff')
+    }
     
 }
 
