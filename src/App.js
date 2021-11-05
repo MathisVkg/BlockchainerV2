c4c5fb78c921f0e3b3393f000ff50311046f5cb9
MathisVkg <mvk181@gmail.com>
2021-11-05 13:47:24 +0100

    fix router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Link
+  Routes
 } from "react-router-dom"
-import './App.css'
 import NavBar from "./components/NavBar"
 import Home from "./components/Home"
 import User from "./components/User"
 import './assets/scss/Base.css'
 
-function App() {
+export default function App() {
   return (
       <Router>
           <NavBar/>
-          <Switch>
+          <Routes>
             <Route exact path="/">
               <Home />
             </Route>
             <Route path="/user">
               <User />
             </Route>
-          </Switch>
+          </Routes>
       </Router>
   )
 }
 
-export default App
