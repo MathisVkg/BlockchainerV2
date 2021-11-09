3133d53f087e4ec0d7b03bb39878cdf38e5e2b90
MathisVkg <mvk181@gmail.com>
2021-11-09 14:05:07 +0100

    sign up page

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import {useState} from "react";
+import './../../assets/scss/Sign.css'
+import { FaFreebsd } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
 
 export default function SignUp() {
     const [username, setUsername] = useState("")
@@ -17,17 +20,45 @@ export default function SignUp() {
     }
 
     return (
-        <div>
-            <div>Error</div>
-            <form onSubmit={e => handleSubmit(e)}>
-                <label htmlFor="username">username :</label>
-                <input onChange={e => setUsername(e.target.value)} type="text" id="username" name="username"/>
-                <label htmlFor="email" >email :</label>
-                <input onChange={e => setEmail(e.target.value)} type="email" id="email" name="email"/>
-                <label htmlFor="password">password :</label>
-                <input onChange={e => setPassword(e.target.value)} type="password" id="password" name="password"/>
-                <input type="submit"/>
-            </form>
-        </div>
+        <>
+            <ContainerInfo />
+            <ContainerForm />
+        </>
     )
+
+
+    function ContainerInfo() {
+        return (
+            <div className="containerOne">
+                <span className="icon"><FaFreebsd /></span>
+                <p className="title">Create your account now on Blockchainer</p>
+                <p className="des">The best app for see all cryptocurrencies and more</p>
+                <p className="des2">Check your wallets and more !</p>
+            </div>
+        )
+    }
+
+    function ContainerForm() {
+        return (
+            <div className="containerTwo">
+                <div className="error">Error</div>
+                <form onSubmit={e => handleSubmit(e)} className="formSign">
+                    <div className="group">
+                        <label htmlFor="username" className="label username">Username</label>
+                        <input onChange={e => setUsername(e.target.value)} type="text" id="username" name="username" placeholder="DarkNetMaster"/>
+                    </div>
+                    <div className="group">
+                        <label htmlFor="email" className="label email">Email</label>
+                        <input onChange={e => setEmail(e.target.value)} type="email" id="email" name="email" placeholder="darkXxX@gmail.com"/>
+                    </div>
+                    <div className="group">
+                        <label htmlFor="password" className="label password">Password</label>
+                        <input onChange={e => setPassword(e.target.value)} type="password" id="password" name="password" placeholder="*********"/>
+                    </div>
+                    <input type="submit" className="submit"/>
+                </form>
+                <p className="already">Already sign ?<NavLink to="/" className="alreadySpan">Sign in here</NavLink></p>
+            </div>
+        )
+    }
 }
