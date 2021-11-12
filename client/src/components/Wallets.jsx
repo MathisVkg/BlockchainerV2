91e60a1e0a321d308afd3934c94633c865333eae
MathisVkg <mvk181@gmail.com>
2021-11-12 09:59:59 +0100

    wallets page

---
+++ client/src/components/Wallets.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './../assets/scss/Wallets.scss'
+
+const Wallets = () => {
+    return (
+        <div>
+            apiKey<input type="text" name="apiKey"/>
+            secretKey<input type="text" name="secretKey"/>
+        </div>
+    )
+}
+
+export default Wallets
