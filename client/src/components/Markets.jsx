9589109167343919f146d4686131bb5b5541a49a
Giuseppe Moi <giuseppemoi@Giuseppes-MacBook-Air.local>
2021-11-16 09:31:02 +0100

    commit

--- client/src/components/Markets.jsx
+++ client/src/components/Markets.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 const Markets = () => {
+    const [data, setData] = useState([])
+
+    useEffect(async () => {
+        await axios.get("http://localhost:3001/data").then((resp) => {
+            setData(resp.data)
+        })
+    },[])
+    console.log(data)
     return (
         <div>
             markets
--- server/routes/Wallets.js
+++ server/routes/Wallets.js
@@ -22,19 +22,7 @@ router.get('/', async function (req, res) {
 
     const result = await client.account()//.then(response => client.logger.log(response.data))
 
-<<<<<<< HEAD
-    return res.send(JSON.stringify(result.data));
-=======
-    // const result = await axios.get('https://api.binance.com/sapi/v1/system/status', {
-    //         headers: {
-    //             'X-MBX-APIKEY': 'n6r2pKPOWD0JUq0nd6kiPmXYvLD4E99QeevDq02zLlPyCu5IO9Bnv4jATgcoFrpx',
-    //             //'HMAC-SHA256': 'STlK6PFIyn5xRzS60ho8YB3gA7VbAuolKtKSBcPWjvD4TSZDOKj8D4uRfKw1h9mE'
-    //         }
-    //     }
-    // )
-    // console.log(result)
     return res.send(result.data)
->>>>>>> 430985eb83ad29f0616ad9183ae95a742d6fd12f
 })
 
 module.exports = router
